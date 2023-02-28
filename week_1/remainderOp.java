cc40f32a7c4266cd1177e85ce6edfeea7babac55
DylanPerkins <dp011400@gmail.com>
2023-02-27 23:25:55 -0500

    Added noted about remainder operator

---
+++ week_1/remainderOp.java
@@ -0,0 +1,15 @@
+package promineo_tech_bootcamp.week_1;
+
+public class remainderOp {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 2;
+
+        System.out.println(num1 / num2); // int divison produces int answer
+        System.out.println(num1 % num2); // Remainder
+    }
+}
+
+
+// Modulo or Remainder Operator (%) returns the ...
+// remainder of the two numbers after division.
