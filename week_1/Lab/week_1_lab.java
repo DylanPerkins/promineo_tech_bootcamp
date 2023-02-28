3a0c9e099be3badc0451f5b75883c163a3f320b3
DylanPerkins <dp011400@gmail.com>
2023-02-27 22:22:06 -0500

    Created W1 Lab folder, and submitted lab work.

---
+++ week_1/Lab/week_1_lab.java
@@ -0,0 +1,48 @@
+package promineo_tech_bootcamp.week_1.Lab;
+
+public class week_1_lab {
+    public static void main(String[] args) {
+        
+        // 1.
+        int planeSeatsAvailable = 10;
+
+        // 2.
+        double costOfGroceries = 22.05;
+
+        // 3.
+        char middleInitial = 'F';
+
+        // 4.
+        boolean isHotOutside = false;
+
+        // 5.
+        String firstName = "Dylan";
+
+        // 6.
+        String streetAddress = "Main Street";
+
+        // 7.
+        System.out.println(planeSeatsAvailable);
+        System.out.println(costOfGroceries);
+        System.out.println(middleInitial);
+        System.out.println(isHotOutside);
+        System.out.println(firstName);
+        System.out.println(streetAddress);
+
+        // 8.
+        planeSeatsAvailable = planeSeatsAvailable - 2;
+
+        // 9.
+        costOfGroceries = costOfGroceries + 2.15;
+
+        // 10.
+        middleInitial = 'M';
+
+        // 11.
+        isHotOutside = true; 
+
+        // 12.
+        System.out.println("This is " + firstName + " and they live on " + streetAddress);
+
+    }
+}
