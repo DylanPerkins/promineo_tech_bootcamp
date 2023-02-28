924e7144b9426a321e832525b24f413a8c0b272e
DylanPerkins <dp011400@gmail.com>
2023-02-27 21:44:41 -0500

    Week 1 notes and learning

---
+++ week_1/first_java.java
@@ -0,0 +1,7 @@
+package promineo_tech_bootcamp.week_1;
+
+public class first_java {
+    public static void main (String[] args) {
+        System.out.println("Hello, World.");
+    }
+}
---
+++ week_1/operations.java
@@ -0,0 +1,28 @@
+package promineo_tech_bootcamp.week_1;
+
+public class operations {
+    public static void main(String[] args) {
+        int age = 34;
+        System.out.println(age);
+        boolean isAge30 = age == 30; // Equality
+        System.out.println(isAge30);
+
+        System.out.println(age == 34);
+
+        age = age + 1; // Addition
+        System.out.println(age);
+
+        age = age * 2; // Multiplication
+        System.out.println(age);
+
+        String firstName = "Dylan";
+        String lastName = "Perkins";
+        String fullName = firstName + " " + lastName; // Concatenation
+        System.out.println(fullName);
+
+    }
+    
+}
+
+// An operator performs actions on operands
+// + - * /
