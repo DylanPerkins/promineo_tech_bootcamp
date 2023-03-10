11fb78544ff21d572b7631c1178f9502f67d7435
DylanPerkins <dp011400@gmail.com>
2023-03-10 13:21:00 -0500

    Arrays Learning

---
+++ week_3/Notes/RobustGradebook/Student.java
@@ -0,0 +1,22 @@
+package week_3.Notes.RobustGradebook;
+
+public class Student {
+
+    String fullName = "";
+    int[] grades;
+
+    public Student(String fName, int[] grades) {
+        this.fullName = fName;
+        this.grades = grades;
+    }
+
+    public void describe() {
+        System.out.println("Student: " + this.fullName);
+        System.out.println("Grades:");
+        for (int grade : this.grades) {
+            System.out.println("\t" + grade + " ");
+        }
+        System.out.println();
+    }
+
+}
