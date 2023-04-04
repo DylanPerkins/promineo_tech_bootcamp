1e963e0d0006a3c98296804c4654140b5088773f
DylanPerkins <dp011400@gmail.com>
2023-04-03 23:05:26 -0400

    Made int input simplier for row/column

--- week_6/Lab/TicTacToe.java
+++ week_6/Lab/TicTacToe.java
@@ -29,9 +29,9 @@ public class TicTacToe {
 
 			// Grab the row and column from the user
 			System.out.println("Player " + currentPlayer + ", please enter a row: ");
-			row = input.nextInt();
+			row = input.nextInt() - 1;
 			System.out.println("Player " + currentPlayer + ", please enter a column: ");
-			column = input.nextInt();
+			column = input.nextInt() -1;
 
 			// Check to see if the row and column are valid
 			if (row < 0 || row > 2 || column < 0 || column > 2) {
