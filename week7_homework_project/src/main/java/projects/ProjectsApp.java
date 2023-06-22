a52b9cf7b1b98202ba339a0b1a876739f07a37e3
DylanPerkins <dp011400@yahoo.com>
2023-06-22 18:52:37 -0400

    finished w9 homework

--- week7_homework_project/src/main/java/projects/ProjectsApp.java
+++ week7_homework_project/src/main/java/projects/ProjectsApp.java
@@ -1,12 +1,16 @@
 package projects;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Objects;
 import java.util.Scanner;
 
+import projects.entity.Project;
 import projects.exception.dbException;
+import projects.service.ProjectService;
 
 public class ProjectsApp {
+	private static final ProjectService projectService = new ProjectService();
 
 	// @formatter:off
 	private List<String> operations = List.of(
@@ -30,6 +34,9 @@ public class ProjectsApp {
 					case -1:
 						done = true;
 						break;
+					case 1:
+						createProject();
+						break;
 					default:
 						System.out.println("\n" + selection + " is not a valid selection. Please try again.");
 						break;
@@ -38,6 +45,41 @@ public class ProjectsApp {
 			} catch (Exception e) {
 				System.out.println("\nError: " + e.getMessage() + ".\nPlease Try again.");
 			}
+		}
+	}
+
+	private void createProject() {
+		String projectName = getStringInput("Enter project name");
+		BigDecimal estimatedHours = getDecimalInput("Enter estimated hours");
+		BigDecimal actualHours = getDecimalInput("Enter actual hours");
+		Integer difficulty = getIntInput("Enter difficulty (1-5)");
+		String notes = getStringInput("Enter notes");
+
+		Project project = new Project();
+
+		project.setProjectName(projectName);
+		project.setEstimatedHours(estimatedHours);
+		project.setActualHours(actualHours);
+		project.setDifficulty(difficulty);
+		project.setNotes(notes);
+
+		Project dbProject = projectService.addProject(project);
+
+		System.out.println("\nCreated project: " + dbProject);
+
+	}
+
+	private BigDecimal getDecimalInput(String prompt) {
+		String input = getStringInput(prompt);
+
+		if (Objects.isNull(input)) {
+			return null;
+		}
+
+		try {
+			return new BigDecimal(input).setScale(2);
+		} catch (NumberFormatException e) {
+			throw new dbException(input + " is not a valid decimal number.");
 		}
 	}
 
