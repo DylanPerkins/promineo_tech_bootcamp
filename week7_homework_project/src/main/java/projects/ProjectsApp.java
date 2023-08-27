200c0981e4ce8bae481b5ee5fd0075f95ff14689
DylanPerkins <dp011400@gmail.com>
2023-08-27 12:57:32 -0400

    converted to spaces, no idea why it was tabs

--- week7_homework_project/src/main/java/projects/ProjectsApp.java
+++ week7_homework_project/src/main/java/projects/ProjectsApp.java
@@ -10,151 +10,151 @@ import projects.exception.dbException;
 import projects.service.ProjectService;
 
 public class ProjectsApp {
-	private static final ProjectService projectService = new ProjectService();
-	private Project curProject;
+    private static final ProjectService projectService = new ProjectService();
+    private Project curProject;
 
-	// @formatter:off
-	private List<String> operations = List.of(
-			"1) Add a new project",
-			"2) List all projects",
-			"3) Select a project"
-	);
-	// @formatter:on
-	private Scanner scanner = new Scanner(System.in);
+    // @formatter:off
+    private List<String> operations = List.of(
+            "1) Add a new project",
+            "2) List all projects",
+            "3) Select a project"
+    );
+    // @formatter:on
+    private Scanner scanner = new Scanner(System.in);
 
-	public static void main(String[] args) {
-		new ProjectsApp().processUserSelection();
-	}
+    public static void main(String[] args) {
+        new ProjectsApp().processUserSelection();
+    }
 
-	private void processUserSelection() {
-		boolean done = false;
+    private void processUserSelection() {
+        boolean done = false;
 
-		while (!done) {
-			try {
-				int selection = obtainUserSelection();
+        while (!done) {
+            try {
+                int selection = obtainUserSelection();
 
-				switch (selection) {
-					case -1:
-						done = exitMenu();
-						break;
-					case 1:
-						createProject();
-						break;
-					case 2:
-						listProjects();
-						break;
-					case 3:
-						selectProject();
-						break;
-					default:
-						System.out.println("\n" + selection + " is not a valid selection. Please try again.");
-						break;
-				}
+                switch (selection) {
+                    case -1:
+                        done = exitMenu();
+                        break;
+                    case 1:
+                        createProject();
+                        break;
+                    case 2:
+                        listProjects();
+                        break;
+                    case 3:
+                        selectProject();
+                        break;
+                    default:
+                        System.out.println("\n" + selection + " is not a valid selection. Please try again.");
+                        break;
+                }
 
-			} catch (Exception e) {
-				System.out.println("\nError: " + e + ".\nPlease try again.");
-			}
-		}
-	}
+            } catch (Exception e) {
+                System.out.println("\nError: " + e + ".\nPlease try again.");
+            }
+        }
+    }
 
-	private void selectProject() {
-		listProjects();
-		Integer projectId = getIntInput("Enter project id");
+    private void selectProject() {
+        listProjects();
+        Integer projectId = getIntInput("Enter project id");
 
-		curProject = null;
+        curProject = null;
 
-		curProject = projectService.fetchProjectById(projectId);
-	}
+        curProject = projectService.fetchProjectById(projectId);
+    }
 
-	private void listProjects() {
-		List<Project> projects = projectService.fetchAllProjects();
+    private void listProjects() {
+        List<Project> projects = projectService.fetchAllProjects();
 
-		System.out.println("\nProjects:");
-		projects.forEach(
-				project -> System.out.println("\t" + project.getProjectId() + ": " + project.getProjectName()));
-	}
+        System.out.println("\nProjects:");
+        projects.forEach(
+                project -> System.out.println("\t" + project.getProjectId() + ": " + project.getProjectName()));
+    }
 
-	private boolean exitMenu() {
-		System.out.println("\nGoodbye!");
-		return true;
-	}
+    private boolean exitMenu() {
+        System.out.println("\nGoodbye!");
+        return true;
+    }
 
-	private void createProject() {
-		String projectName = getStringInput("Enter project name");
-		BigDecimal estimatedHours = getDecimalInput("Enter estimated hours");
-		BigDecimal actualHours = getDecimalInput("Enter actual hours");
-		Integer difficulty = getIntInput("Enter difficulty (1-5)");
-		String notes = getStringInput("Enter notes");
+    private void createProject() {
+        String projectName = getStringInput("Enter project name");
+        BigDecimal estimatedHours = getDecimalInput("Enter estimated hours");
+        BigDecimal actualHours = getDecimalInput("Enter actual hours");
+        Integer difficulty = getIntInput("Enter difficulty (1-5)");
+        String notes = getStringInput("Enter notes");
 
-		Project project = new Project();
+        Project project = new Project();
 
-		project.setProjectName(projectName);
-		project.setEstimatedHours(estimatedHours);
-		project.setActualHours(actualHours);
-		project.setDifficulty(difficulty);
-		project.setNotes(notes);
+        project.setProjectName(projectName);
+        project.setEstimatedHours(estimatedHours);
+        project.setActualHours(actualHours);
+        project.setDifficulty(difficulty);
+        project.setNotes(notes);
 
-		Project dbProject = projectService.addProject(project);
+        Project dbProject = projectService.addProject(project);
 
-		System.out.println("\nCreated project: " + dbProject);
+        System.out.println("\nCreated project: " + dbProject);
 
-	}
+    }
 
-	private BigDecimal getDecimalInput(String prompt) {
-		String input = getStringInput(prompt);
+    private BigDecimal getDecimalInput(String prompt) {
+        String input = getStringInput(prompt);
 
-		if (Objects.isNull(input)) {
-			return null;
-		}
+        if (Objects.isNull(input)) {
+            return null;
+        }
 
-		try {
-			return new BigDecimal(input).setScale(2);
-		} catch (NumberFormatException e) {
-			throw new dbException(input + " is not a valid decimal number.");
-		}
-	}
+        try {
+            return new BigDecimal(input).setScale(2);
+        } catch (NumberFormatException e) {
+            throw new dbException(input + " is not a valid decimal number.");
+        }
+    }
 
-	private int obtainUserSelection() {
-		printOperations();
+    private int obtainUserSelection() {
+        printOperations();
 
-		Integer input = getIntInput("\nPlease select an operation");
+        Integer input = getIntInput("\nPlease select an operation");
 
-		return Objects.isNull(input) ? -1 : input;
-	}
+        return Objects.isNull(input) ? -1 : input;
+    }
 
-	private Integer getIntInput(String prompt) {
-		String input = getStringInput(prompt);
+    private Integer getIntInput(String prompt) {
+        String input = getStringInput(prompt);
 
-		if (Objects.isNull(input)) {
-			return null;
-		}
+        if (Objects.isNull(input)) {
+            return null;
+        }
 
-		try {
-			return Integer.valueOf(input);
-		} catch (NumberFormatException e) {
-			throw new dbException(input + " is not a valid integer.");
-		}
-	}
+        try {
+            return Integer.valueOf(input);
+        } catch (NumberFormatException e) {
+            throw new dbException(input + " is not a valid integer.");
+        }
+    }
 
-	private String getStringInput(String prompt) {
-		System.out.print(prompt + ": ");
-		String input = scanner.nextLine();
+    private String getStringInput(String prompt) {
+        System.out.print(prompt + ": ");
+        String input = scanner.nextLine();
 
-		return input.isBlank() ? null : input.trim();
-	}
+        return input.isBlank() ? null : input.trim();
+    }
 
-	private void printOperations() {
-		System.out.println();
-		System.out.println("Here's what you can do: (Press 'Enter' to quit)");
+    private void printOperations() {
+        System.out.println();
+        System.out.println("Here's what you can do: (Press 'Enter' to quit)");
 
-		operations.forEach(op -> System.out.println("\t" + op));
+        operations.forEach(op -> System.out.println("\t" + op));
 
-		if (Objects.isNull(curProject)) {
-			System.out.println("\nNo Selected project found.");
-		} else {
-			System.out.println("\nSelected project: " + curProject);
-		}
+        if (Objects.isNull(curProject)) {
+            System.out.println("\nNo Selected project found.");
+        } else {
+            System.out.println("\nSelected project: " + curProject);
+        }
 
-	}
+    }
 
 }
