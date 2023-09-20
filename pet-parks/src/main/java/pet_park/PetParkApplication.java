b6ebb638a474794d66c94949288d7d72df57d963
DylanPerkins <dp011400@gmail.com>
2023-09-19 22:15:26 -0400

    Finished 1st video of week 13

---
+++ pet-parks/src/main/java/pet_park/PetParkApplication.java
@@ -0,0 +1,11 @@
+package pet_park;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetParkApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PetParkApplication.class, args);
+	}
+}
