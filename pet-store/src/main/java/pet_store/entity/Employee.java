878b9d6b1447b59d1fee37c937238fc84a30351e
DylanPerkins <dp011400@yahoo.com>
2023-09-27 21:31:03 -0400

    Finished w13 coding assignment.
    Made the pet-store project, connected it to it's schema and verified it works

---
+++ pet-store/src/main/java/pet_store/entity/Employee.java
@@ -0,0 +1,31 @@
+package pet_store.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@Entity
+@Data
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long employeeId;
+
+    private String employeeFirstName;
+    private String employeeLastName;
+    private String employeePhone;
+    private String employeeJobTitle;
+
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "pet_store_id")
+    private PetStore petStore;
+}
