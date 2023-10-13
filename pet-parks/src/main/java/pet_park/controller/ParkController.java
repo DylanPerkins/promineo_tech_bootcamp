3acb3b940c7bfce24f94e4775b8c00927cbc9e21
DylanPerkins <dp011400@gmail.com>
2023-10-13 15:09:47 -0400

    PUTs are now possible

--- pet-parks/src/main/java/pet_park/controller/ParkController.java
+++ pet-parks/src/main/java/pet_park/controller/ParkController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -30,6 +31,15 @@ public class ParkController {
         return parkService.saveContributor(contributorData);
     }
 
+    @PutMapping("/contributor/{contributorId}")
+    public ContributorData updateContributor(@PathVariable Long contributorId,
+            @RequestBody ContributorData contributorData) {
+        contributorData.setContributorId(contributorId);
+        log.info("Updating contributor {}", contributorData);
+
+        return parkService.saveContributor(contributorData);
+    }
+
     @GetMapping("/contributor")
     public List<ContributorData> retrieveAllContributors() {
         log.info("Retrieve all contributors called.");
@@ -40,7 +50,6 @@ public class ParkController {
     public ContributorData retrieveContributorById(@PathVariable Long contributorId) {
         log.info("Retrieving contributor with ID={}", contributorId);
         return parkService.retrieveContributorById(contributorId);
-
 
     }
 }
