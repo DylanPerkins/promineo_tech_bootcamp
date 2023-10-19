df8d334eaf507330448daa2ca6576b1e254c2e46
DylanPerkins <dp011400@yahoo.com>
2023-10-18 21:09:25 -0400

    Removed extra import

--- pet-store/src/main/java/pet_store/service/PetStoreService.java
+++ pet-store/src/main/java/pet_store/service/PetStoreService.java
@@ -5,7 +5,6 @@ import java.util.Objects;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import pet_store.controller.model.PetStoreData;
 import pet_store.dao.PetStoreDao;
