d452c81c8e7c82bc186fb8e239ced6056e66ffb5
DylanPerkins <dp011400@gmail.com>
2023-12-09 13:22:10 -0500

    New Maven Project, for w15 videos

---
+++ dog-rescue/src/main/java/dog_rescue/DogRescueApplication.java
@@ -0,0 +1,11 @@
+package dog_rescue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogRescueApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DogRescueApplication.class, args);
+    }
+}
