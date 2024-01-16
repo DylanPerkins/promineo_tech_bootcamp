979be55b494cf1301831aff0109a9b90358ddc28
DylanPerkins <dp011400@yahoo.com>
2024-01-16 16:33:33 -0500

    Update .gitignore and Users.java

--- anime-reviews/src/main/java/anime_reviews/entity/Users.java
+++ anime-reviews/src/main/java/anime_reviews/entity/Users.java
@@ -1,5 +1,7 @@
 package anime_reviews.entity;
 
+import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import jakarta.persistence.CascadeType;
@@ -30,16 +32,16 @@ public class Users {
     private String username;
 
     @Column(name = "watched_anime")
-    private Anime watchedAnime;
+    private List<Integer> watchedAnime;
 
     @Column(name = "watching_anime")
-    private Anime watchingAnime;
+    private List<Integer> watchingAnime;
 
     @Column(name = "want_to_watch")
-    private Anime wantToWatch;
+    private List<Integer> wantToWatch;
 
     @Column(name = "wont_watch")
-    private Anime wontWatch;
+    private List<Integer> wontWatch;
 
     @OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
     private AnimeReview animeReview;
@@ -48,5 +50,5 @@ public class Users {
     @ToString.Exclude
     @ManyToMany(cascade = CascadeType.PERSIST)
     @JoinTable(name = "anime_users", joinColumns = @JoinColumn(name = "anime_id"), inverseJoinColumns = @JoinColumn(name = "user_id"))
-    private Set<Anime> anime;
+    private Set<Anime> anime = new HashSet<>();
 }
